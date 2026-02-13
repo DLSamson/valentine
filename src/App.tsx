d2f70ac5f29a5889e7ea727dd46db90d528d45a3
dgarmash <dgarmash@synergy.ru>
2026-02-13 18:08:20 +0300

    add deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Container } from "./components/layouts/Container";
 import { PageLayout } from "./components/layouts/PageLayout";
 import { HeartRain } from "./components/HeartRain";
-import { Routes, BrowserRouter, Route } from "react-router"
+import { Routes, Route, HashRouter, Link } from "react-router"
 
 const Page1 = () => (<p>hi!</p>);
 const Page2 = () => (<p>hi!</p>);
@@ -11,17 +11,18 @@ const Page5 = () => (<p>hi!</p>);
 const Page6 = () => (<p>hi!</p>);
 
 export const App = () => (
-    <BrowserRouter>
+    <HashRouter>
         <PageLayout>
             <Container>
                 <Routes>
-                    <Route path="/1" element={Page1} />
-                    <Route path="/2" element={Page2} />
-                    <Route path="/3" element={Page3} />
-                    <Route path="/4" element={Page4} />
-                    <Route path="/5" element={Page5} />
-                    <Route path="/6" element={Page6} />
+                    <Route path="/1" element={<Page1 />} />
+                    <Route path="/2" element={<Page2 />} />
+                    <Route path="/3" element={<Page3 />} />
+                    <Route path="/4" element={<Page4 />} />
+                    <Route path="/5" element={<Page5 />} />
+                    <Route path="/6" element={<Page6 />} />
                 </Routes>
+                <Link to="/1"> 123</Link>
             </Container>
 
             <HeartRain
@@ -31,5 +32,5 @@ export const App = () => (
                 className="z-50"
             />
         </PageLayout>
-    </BrowserRouter>
+    </HashRouter>
 );
